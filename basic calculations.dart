4842fd5735ab97d8b58a2e9a8d3541f60dedbdf6
Inzy Y <neerajkeeriyad@gmail.com>
2025-01-07 10:51:16 +0530

    dart basic calculations

---
+++ basic calculations.dart
@@ -0,0 +1,12 @@
+void main() {
+  int num1 = 10;
+  int num2 = 20;
+  int sum = num1 + num2;
+  int multi = num1 * num2;
+  int diff = num1 - num2;
+  double div = num1 / num2;
+  print("sum is $sum");
+  print("multi is $multi");
+  print("diff is $diff");
+  print("div is $div");
+}
